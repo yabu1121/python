2e2dc2f09d191099c14c94e22ac4072a70dd6958
yabu1121 <n.hayato0122@outlook.jp>
2025-11-22 17:56:35 +0900

    numerate

--- todo.py
+++ todo.py
@@ -1,4 +1,6 @@
-todo_list = []
+todo_list = [
+  {"task": "demo 1", "done": False},
+]
 
 def display_menu():
   print("\n TODO-APP")
@@ -9,46 +11,49 @@ def display_menu():
   print("0: アプリを終了")
   print("-----------")
 
-def add_todo():
-  print("add")
-  choice()
+def add_todo(t_list):
+  task = input("追加したいタスクを入力してください。\n")
+  t_list.append({"task": task, "done": False})
+  print("タスクを追加しました。")
 
-def display_todo():
-  print("display")
-  choice()
+def display_todo(t_list):
+  for index, item in enumerate(t_list):
+    task_state = item["done"] == True if "✅" else "未"
+    print(f"{index + 1} : {item["task"]} {task_state}")
+  print("todoリストを出力しました。")
 
-def complete_todo():
+def complete_todo(t_list):
   print("complete")
-  choice()
-  
-def delete_todo():
+
+def delete_todo(t_list):
+  print("どのタスクを削除しますか")
+  complete_task_index = input("番号で教えてください。")
+  complete_task_index = int(complete_task_index) - 1
+  print(t_list[complete_task_index] + "を削除しました。")
+  t_list.pop(complete_task_index)
   print("delete")
-  choice()
 
 def end_todo():
-  print("end")
-  choice()
-
-def choice():
-  choice = input("0-4で選択してください")
-  if choice == '1':
-    add_todo()
-  elif choice == '2':
-    display_todo()
-  elif choice == '3':
-    complete_todo()
-  elif choice == '4':
-    delete_todo()
-  elif choice == '0':
-    end_todo()
-  else:
-    print("無効な入力です。終了したい場合は0を入力してください。")
-    choice() 
-
+  print("todoアプリを終了します。")
 
 def main():
-  display_menu()
-  choice()
+  global todo_list
+  while(True):
+    display_menu()
+    user_choice = input("0-4で選択してください\n")
+    if user_choice == '1':
+      add_todo(todo_list)
+    elif user_choice == '2':
+      display_todo(todo_list)
+    elif user_choice == '3':
+      complete_todo(todo_list)
+    elif user_choice == '4':
+      delete_todo(todo_list)
+    elif user_choice == '0':
+      end_todo()
+      break
+    else:
+      print("無効な入力です。終了したい場合は0を入力してください。") 
 
 if __name__ == "__main__":
     main()
